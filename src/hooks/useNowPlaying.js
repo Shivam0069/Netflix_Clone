87f9512d6412298d1ee9abf2c1ebb3e8897b1902
Shivam0069 <shivamsinghchauhan7000@gmail.com>
2024-01-25 21:54:53 +0530

    Fetching data from TMDB server

--- src/hooks/useNowPlaying.js
+++ src/hooks/useNowPlaying.js
@@ -9,21 +9,21 @@ const useNowPlaying = () => {
   useEffect(() => {
     const getNowPlayingMovies = async () => {
       //Fetching Data from External API to get movie data and storing it in firebase 
-      // const data = await fetch(
-      //   "https://api.themoviedb.org/3/movie/now_playing?page=1",
-      //   options
-      // );
-      // const nowplaying = await data.json();
-      // fetch(
-      //   "https://netflix-project-708e8-default-rtdb.firebaseio.com/nowplaying.json",
-      //   {
-      //     method: "PUT",
-      //     headers: {
-      //       "Content-Type": "application/json",
-      //     },
-      //     body: JSON.stringify(nowplaying.results),
-      //   }
-      // );
+      const data = await fetch(
+        "https://api.themoviedb.org/3/movie/now_playing?page=1",
+        options
+      );
+      const nowplaying = await data.json();
+      fetch(
+        "https://netflix-project-708e8-default-rtdb.firebaseio.com/nowplaying.json",
+        {
+          method: "PUT",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify(nowplaying.results),
+        }
+      );
       const movieData = await fetch(
         "https://netflix-project-708e8-default-rtdb.firebaseio.com/nowplaying.json"
       );
--- src/hooks/useUpcomingMovies.js
+++ src/hooks/useUpcomingMovies.js
@@ -10,21 +10,21 @@ const useUpcomingMovies = () => {
     const getUpcomingMovies = async () => {
       //Fetching Data from External API to get movie data and storing it in firebase
 
-      // const data = await fetch(
-      //   "https://api.themoviedb.org/3/movie/upcoming?page=1",
-      //   options
-      // );
-      // const upcomingMovies = await data.json();
-      // fetch(
-      //   "https://netflix-project-708e8-default-rtdb.firebaseio.com/upcomingmovies.json",
-      //   {
-      //     method: "PUT",
-      //     headers: {
-      //       "Content-Type": "application/json",
-      //     },
-      //     body: JSON.stringify(upcomingMovies.results),
-      //   }
-      // );
+      const data = await fetch(
+        "https://api.themoviedb.org/3/movie/upcoming?page=1",
+        options
+      );
+      const upcomingMovies = await data.json();
+      fetch(
+        "https://netflix-project-708e8-default-rtdb.firebaseio.com/upcomingmovies.json",
+        {
+          method: "PUT",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify(upcomingMovies.results),
+        }
+      );
       const movieData = await fetch(
         "https://netflix-project-708e8-default-rtdb.firebaseio.com/upcomingmovies.json"
       );
