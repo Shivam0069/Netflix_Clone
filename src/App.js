1460404f86cf1dd6f7fa2c14f9959c5aca81f63c
Shivam0069 <shivamsinghchauhan7000@gmail.com>
2024-01-25 18:57:00 +0530

    Completed the Routes

---
+++ src/App.js
@@ -0,0 +1,29 @@
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import Login from "./components/Login";
+import Browse from "./components/Browse";
+import Home from "./components/Home";
+
+function App() {
+  const appRouter = createBrowserRouter([
+    {
+      path: "/",
+      element: <Home />,
+    },
+    {
+      path: "/browse",
+      element: <Browse />,
+    },
+    {
+      path: "/login",
+      element: <Login />,
+    },
+  ]);
+
+  return (
+    <div className="scroll-smooth">
+      <RouterProvider router={appRouter} />
+    </div>
+  );
+}
+
+export default App;
