d6d1a1f4d3837c4b7c9b046a001235c4caa21cf1
Shivam0069 <shivamsinghchauhan7000@gmail.com>
2024-01-25 21:58:45 +0530

    Animated the videoTitle buttons

--- src/components/VideoTitle.js
+++ src/components/VideoTitle.js
@@ -7,10 +7,10 @@ const VideoTitle = ({ title }) => {
       <h1 className="lg:text-5xl md:text-4xl text-3xl w-[500px] font-bold text-[#BABCBE]">{title}</h1>
      
       <div className="flex gap-2 mt-5">
-        <button className="lg:px-10 md:px-8 px-7 rounded-md lg:py-2 py-1 bg-white text-black lg:text-xl md:text-lg text-md">
+        <button className="lg:px-10 md:px-8 px-7 hover:scale-110 rounded-md lg:py-2 py-1 bg-white text-black lg:text-xl md:text-lg text-md">
           Play
         </button>
-        <button className="lg:px-8 md:px-6 px-5  rounded-md lg:py-2 py-1 bg-black bg-opacity-50 md:text-lg text-md">
+        <button className="lg:px-8 md:px-6 px-5 hover:scale-110 hover:brightness-125  rounded-md lg:py-2 py-1 bg-black bg-opacity-50 md:text-lg text-md">
           More info
         </button>
       </div>
